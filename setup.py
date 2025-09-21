194f639beeaaf7300145522615da28f66624e97e
ljlazar <68371125+ljlazar@users.noreply.github.com>
2025-09-21 02:57:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 REQUIREMENTS = [
-    "IPython"
+    "IPython",
     "pandas",
     "numpy",
     "thermo",
@@ -44,4 +44,5 @@ setup(
     install_requires=REQUIREMENTS,
 
 )
+
 
